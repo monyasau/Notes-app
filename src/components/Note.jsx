d9a99fb25a5e67aa513229911d3cebe73926ee41
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-01-11 06:31:02 +0100

    update: attempted to create seperate view for notes

---
+++ src/components/Note.jsx
@@ -0,0 +1,16 @@
+let Note=({noteData})=> {
+    return (<>
+              <div
+          aria-label="backdrop"
+          className=" fixed inset-0 z-40 bg-black/20 backdrop-blur-[1px] dark:bg-slate-900/80"
+        ></div>
+        <div key={noteData.id} className="p-4 z-50 top-0 fixed border my-2  bg-gray-300 w-[90%] mx-auto overflow-y-hidden">
+            {noteData.id}
+        <h1 className=" text-xl">{noteData.title}</h1>
+        <span>Note: {noteData.content}</span>
+      </div>
+      </>
+    );
+}
+
+export default Note;
--- src/components/NoteMainApp.jsx
+++ src/components/NoteMainApp.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+import Note from "./Note";
+
 let NoteMainApp = () => {
+  const [focusNote, setFocusNote] = useState(false);
+  const [focusedNote, setFocusedNote] = useState(null);
   let notesTemplate = [
     {
       id: 1,
@@ -31,16 +36,25 @@ let NoteMainApp = () => {
         "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur deserunt aliquam sit eaque nam soluta assumenda maxime deleniti fuga cum eligendi recusandae alias, quibusdam optio sint facilis voluptatibus blanditiis adipisci?",
     },
   ];
-
   return (
     <div className="my-4 px-4 border">
       {notesTemplate.length} Notes Available:
       {notesTemplate.map((note) => (
-        <div key={note.id} className="p-4 border my-2 h-[20vh] overflow-y-hidden">
-          <h1 className="border ">Subject: {note.title}</h1>
-          <span>Note: {note.content}</span>
+        <div key={note.id}>
+          <div
+            className="p-4 border my-2 h-[20vh] overflow-y-hidden"
+            onClick={() => {
+              setFocusNote(true);
+              setFocusedNote(note.id);
+            }}
+          >
+            Note {note.id}
+            <h1 className=" text-xl">{note.title}</h1>
+            <span>Note: {note.content}</span>
+          </div>
         </div>
       ))}
+      {focusNote ? <Note noteData={notesTemplate[focusedNote]} /> : <></>}
     </div>
   );
 };
