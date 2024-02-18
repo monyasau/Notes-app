7fc3f387b9f1bae838d6f7a7dcb0889c47d3fa16
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-02-18 12:15:53 +0100

    added a save to localstorage button

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,6 +1,7 @@
-let Note=({noteData})=> {
-    return (<>
-              {/* <div
+let Note = ({ noteData }) => {
+  return (
+    <>
+      {/* <div
           aria-label="backdrop"
           className=" fixed inset-0 z-40 bg-black/20 backdrop-blur-[1px] dark:bg-slate-900/80"
         ></div>
@@ -9,23 +10,29 @@ let Note=({noteData})=> {
         <h1 className=" text-xl">{noteData.title}</h1>
         <span>Note: {noteData.content}</span>
       </div> */}
-      
+
       <div
-            className="p-4 border my-2 min-h-[200px] max-h-[200px] overflow-y-scroll"
-            // onClick={() => {
-            //   setFocusNote(true);
-            //   setFocusedNote(note.id);
-            // }}
-            onClick={()=>{
-              let stringifiedNoteData = JSON.stringify(noteData);
-              localStorage.setItem(`data${noteData.id}`, stringifiedNoteData)}}
-          >
-            Note {noteData.id}
-            <h1 className=" text-xl">{noteData.title}</h1>
-            <span>Note: {noteData.content}</span>
-          </div>
-      </>
-    );
-}
+        className="p-4 border my-2 min-h-[200px] max-h-[200px] overflow-y-scroll"
+        // onClick={() => {
+        //   setFocusNote(true);
+        //   setFocusedNote(note.id);
+        // }}
+      >
+        <button
+          onClick={() => {
+            let stringifiedNoteData = JSON.stringify(noteData);
+            localStorage.setItem(`data${noteData.id}`, stringifiedNoteData);
+          }}
+          className="border p-2 mr-3"
+        >
+          Add to local storage
+        </button>
+        Note {noteData.id}
+        <h1 className=" text-xl">{noteData.title}</h1>
+        <span>Note: {noteData.content}</span>
+      </div>
+    </>
+  );
+};
 
 export default Note;
