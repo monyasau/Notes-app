55e17eb764471e0f7d4a5e83eeaced84dd11c3f5
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-02-24 12:01:32 +0100

    improved styling

--- src/components/NoteMainApp.jsx
+++ src/components/NoteMainApp.jsx
@@ -39,7 +39,7 @@ let NoteMainApp = () => {
   return (
     <>
       <div className="my-4 p-4 border bg-slate-50 max-w-screen-xl mx-auto">
-        <div className="flex bg-white p-4">
+        <div className="flex bg-white rounded p-4">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="26"
@@ -50,7 +50,7 @@ let NoteMainApp = () => {
           <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5" />
           <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
         </svg>
-<input type="text" className="w-full border" />
+<input type="text" className="w-full focus:border-none" />
         </div>
       </div>
       <div className="my-4 px-4 border max-w-screen-xl mx-auto">
