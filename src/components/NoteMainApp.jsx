bbdee37d24167ba922e2a67576dc06f05fe3e46e
Olajide Olanrewaju <Olajide.m.olanrewaju@gmail.com>
2024-03-12 02:20:00 +0100

    added title input to new note section

--- src/components/NoteMainApp.jsx
+++ src/components/NoteMainApp.jsx
@@ -38,9 +38,20 @@ let NoteMainApp = () => {
   ];
   return (
     <>
-      <div className="my-4 px-4 border max-w-screen-xl mx-auto">
-        <span>Add a new note</span>
-        <div></div>
+      <div className="my-4 p-4 border bg-slate-50 max-w-screen-xl mx-auto">
+        <div className="flex bg-white p-4">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="26"
+          height="26"
+          fill="currentColor"
+          viewBox="0 0 16 16"
+        >
+          <path d="M8 6.5a.5.5 0 0 1 .5.5v1.5H10a.5.5 0 0 1 0 1H8.5V11a.5.5 0 0 1-1 0V9.5H6a.5.5 0 0 1 0-1h1.5V7a.5.5 0 0 1 .5-.5" />
+          <path d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5zm-3 0A1.5 1.5 0 0 1 9.5 3V1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V4.5z" />
+        </svg>
+<input type="text" className="w-full border" />
+        </div>
       </div>
       <div className="my-4 px-4 border max-w-screen-xl mx-auto">
         <div>
