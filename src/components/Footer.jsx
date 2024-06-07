017fa12b082c6715c51a53e4e478c346ff0b5543
Olajide Olanrewaju <olajide.m.olanrewaju@gmail.com>
2024-06-07 21:21:32 +0100

    Added footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+export default function Footer() {
+
+  const [menu, setMenu] = useState(false);
+
+  return (
+    <>
+    <footer>
+      
+    </footer>
+    </>
+  );
+}
